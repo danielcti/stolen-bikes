29e6978a8178f88fd6bc652a81279bdb1b73b13f
Daniel Cavalcanti <dpc2@cin.ufpe.br>
2021-08-20 00:34:37 -0300

    wip
    Signed-off-by: Daniel Cavalcanti <dpc2@cin.ufpe.br>

--- src/pages/api/bikes/index.ts
+++ src/pages/api/bikes/index.ts
@@ -3,32 +3,33 @@ import { NextApiRequest, NextApiResponse } from "next";
 import Bike from "../../../models/Bike";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-    const client = await connectToDatabase();
+  const client = await connectToDatabase();
 
-    if (client) {
-        switch (req.method) {
-            case "GET":
-                return getBikes();
-            case "POST":
-                return createBike();
-            default:
-                return res.status(405).end(`Method ${req.method} Not Allowed`);
-        }
+  if (client) {
+    switch (req.method) {
+      case "GET":
+        return getBikes();
+      case "POST":
+        return createBike();
+      default:
+        return res.status(405).end(`Method ${req.method} Not Allowed`);
     }
+  }
 
-    async function getBikes() {
-        const bikes = await Bike.find().sort({ date_stolen: 'desc' });
-        return res.status(200).json(bikes);
+  async function getBikes() {
+    const bikes = await Bike.find().sort({ date_stolen: "desc" });
+    return res.status(200).json(bikes);
+  }
+
+  async function createBike() {
+    try {
+      console.log(req.body);
+      const user = await Bike.create(req.body);
+      return res.status(200).json(user);
+    } catch (error) {
+      return res.status(400).json({ message: error });
     }
+  }
 
-    async function createBike() {
-        try {
-            const user = await Bike.create(req.body);
-            return res.status(200).json(user);
-        } catch (error) {
-            return res.status(400).json({ message: error });
-        }
-    }
-
-    return res.status(500);
+  return res.status(500);
 };
